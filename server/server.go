f299f641b593f9dd240bfab36ba7c4377013006a
mtanng9 <mlt152498@gmail.com>
2025-06-29 22:13:23 -0400

    installed tailwind via cdn

--- server/server.go
+++ server/server.go
@@ -1,16 +1,20 @@
 package server
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"path"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello there")
+	filePath := path.Join("web/html", "index.html")
+	template, _ := template.ParseFiles(filePath)
+	template.Execute(w, nil)
 }
 
 func StartServer() {
 	http.HandleFunc("/", index)
+	log.Println("Starting server on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
